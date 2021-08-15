144f6e6fd5fb3414537bb15138c68a8121f2946f
tavs <lucastavaresa95@gmail.com>
2021-08-15 13:41:37 -0300

    Entendendo funções 01

---
+++ function01.js
@@ -0,0 +1,34 @@
+function favoriteAnimal(pet) {
+    console.log("Meu animal favorito é um(a):" + pet)
+}
+
+favoriteAnimal('Cachorro')
+
+function add7(number) {
+    console.log(number+7)
+}
+
+add7(3)
+
+function multiply(num1, num2) {
+    console.log(num1*num2)
+}
+
+multiply(3, 5)
+
+function capitalize(text) {
+    str1 = text.charAt(0).toUpperCase(); 
+    str2 = text.slice(1);
+    result = str1 + str2.toLowerCase();
+    console.log(result)
+}
+
+capitalize('tavs, Tavs, JeeRddR')
+
+function lastLetter(text2) {
+    string1 = text2.slice(-1)
+    console.log(string1)
+}
+
+lastLetter('Rosinha')
+
